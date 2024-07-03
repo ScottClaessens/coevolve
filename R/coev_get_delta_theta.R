b49e26cb3c4464e24f0ec123d7a2d5f03273fcfe
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-07-03 20:09:39 +0100

    Update DESCRIPTION and package dependencies

--- R/coev_get_delta_theta.R
+++ R/coev_get_delta_theta.R
@@ -59,7 +59,7 @@ coev_get_delta_theta <- function(object, response, predictor) {
     stop2("Argument 'response' and 'predictor' must refer to different variables.")
   }
   # extract posterior draws
-  draws <- as_draws_rvars(object$fit$draws())
+  draws <- posterior::as_draws_rvars(object$fit$draws())
   # get variables
   A <- draws$A
   b <- draws$b
@@ -69,10 +69,10 @@ coev_get_delta_theta <- function(object, response, predictor) {
   # medians and median absolute deviations for both variables
   eta_resp <- draws$eta[1:nrow(object$data), id_resp]
   eta_pred <- draws$eta[1:nrow(object$data), id_pred]
-  med_resp <- rvar_median(eta_resp)
-  med_pred <- rvar_median(eta_pred)
-  diff_resp <- rvar_mad(eta_resp)
-  diff_pred <- rvar_mad(eta_pred)
+  med_resp <- posterior::rvar_median(eta_resp)
+  med_pred <- posterior::rvar_median(eta_pred)
+  diff_resp <- posterior::rvar_mad(eta_resp)
+  diff_pred <- posterior::rvar_mad(eta_pred)
   # calculate theta for response given value of predictor
   calculate_theta <- function(pred_value) {
     -( pred_value * A[id_resp,id_pred] + b[id_resp] ) / A[id_resp,id_resp]
@@ -82,7 +82,7 @@ coev_get_delta_theta <- function(object, response, predictor) {
   diff_theta <- calculate_theta(med_pred + diff_pred)
   delta_theta <- (diff_theta - med_theta) / diff_resp
   # save as draws array for output
-  delta_theta <- as_draws_array(delta_theta)
+  delta_theta <- posterior::as_draws_array(delta_theta)
   dimnames(delta_theta)$variable <- "delta_theta"
   return( delta_theta )
 }
