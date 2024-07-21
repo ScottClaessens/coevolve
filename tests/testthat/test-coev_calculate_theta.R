ccf800c865ef9ae915ccb38bdf29ad4a3722fdc9
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-07-21 20:57:52 +0100

    Start writing tests for coev_calculate_theta()

---
+++ tests/testthat/test-coev_calculate_theta.R
@@ -0,0 +1,75 @@
+test_that("coev_calculate_theta() produces expected errors and output", {
+  # load model
+  m <- readRDS(test_path("fixtures", "coevfit_example4.rds"))
+  m <- reload_fit(m, filename = "coevfit_example1-4.csv")
+  # expect the following errors
+  expect_error(
+    coev_calculate_theta(object = "fail"),
+    "Argument 'object' must be a fitted coevolutionary model of class coevfit."
+  )
+  expect_error(
+    coev_calculate_theta(object = m, intervention_values = list("fail")),
+    "Argument 'intervention_values' is not a named list."
+  )
+  expect_error(
+    coev_calculate_theta(object = m, intervention_values = list(fail = NA)),
+    paste0(
+      "At least one variable in 'intervention_values' is not included in ",
+      "the fitted model."
+    )
+  )
+  expect_error(
+    coev_calculate_theta(object = m, intervention_values = list(y = NA)),
+    paste0(
+      "At least one coevolving variable in the model is not included in ",
+      "argument 'intervention_values'."
+    )
+  )
+  expect_error(
+    coev_calculate_theta(
+      object = m,
+      intervention_values = list(y = NA, y = NA, z = NA)
+    ),
+    "Argument 'intervention_values' contains duplicated variable names."
+  )
+  expect_error(
+    coev_calculate_theta(
+      object = m,
+      intervention_values = list(y = 0, z = 0)
+    ),
+    paste0(
+      "Argument 'intervention_values' must have at least one NA value ",
+      "declaring a free variable. If all variables are held constant, the ",
+      "system is already at equilibrium and there is nothing to compute."
+    )
+  )
+  expect_error(
+    coev_calculate_theta(
+      object = m,
+      intervention_values = list(y = NA, z = NA)
+    ),
+    paste0(
+      "Argument 'intervention_values' must have at least one variable ",
+      "held constant (i.e., all values are NA)."
+    )
+  )
+  # should run without error
+  theta1 <-
+    coev_calculate_theta(
+      object = m,
+      intervention_values = list(y = 0, z = NA)
+      )
+  theta2 <-
+    coev_calculate_theta(
+      object = m,
+      intervention_values = list(y = NA, z = 0)
+    )
+  expect_no_error(theta1)
+  expect_no_error(theta2)
+  # output should be matrix of posterior draws
+  expect_true(methods::is(theta1, "matrix"))
+  expect_true(methods::is(theta2, "matrix"))
+  # variables should be correctly held constant in output
+  expect_true(unique(theta1[,"y"]) == 0)
+  expect_true(unique(theta1[,"z"]) == 0)
+})
