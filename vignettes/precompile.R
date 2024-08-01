12afe38289e690e71f282d6f7570c04bb92fe4f7
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-08-01 11:07:28 +0100

    Start updating vignette

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,4 +1,4 @@
 # Pre-compiled vignettes that take a long time to run
-# Must manually move image files from coevolve/ to coevolve/vignettes/ after knit
-# see: https://ropensci.org/blog/2019/12/08/precompute-vignettes/
+# Must manually move image files from coevolve/ to coevolve/vignettes/ after
+# knit, see: https://ropensci.org/blog/2019/12/08/precompute-vignettes/
 knitr::knit("vignettes/coevolve.Rmd.orig", "vignettes/coevolve.Rmd")
