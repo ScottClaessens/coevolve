d374f49dccf1f6e291754d0ba2d75c5bcb3ea377
Scott Claessens <scla896@UoA.auckland.ac.nz>
2024-08-08 13:59:59 +0100

    Add standata method

--- R/stancode.R
+++ R/stancode.R
@@ -1,6 +1,8 @@
-#' Expose the stancode of a fitted model represented by a \code{coevfit} object
+#' Expose the Stan code of a fitted model represented by a \code{coevfit} object
 #'
 #' @param object An object of class \code{coevfit}.
+#'
+#' @return Printed Stan code
 #' @export
 stancode <- function(object){
   UseMethod("stancode")
---
+++ R/standata.R
@@ -0,0 +1,15 @@
+#' Expose the Stan data list for a fitted model represented by a \code{coevfit}
+#'   object
+#'
+#' @param object An object of class \code{coevfit}.
+#'
+#' @return Named list of data for Stan
+#' @export
+standata <- function(object){
+  UseMethod("standata")
+}
+
+#' @export
+standata.coevfit <- function(object){
+  object$stan_data
+}
